6a8a32c36a36139fccf2e07a78d46c9703d29b92
HnDK0 <212071365+HnDK0@users.noreply.github.com>
2026-02-16 21:16:41 +0300

    update

--- core/src/main/java/my/noveldokusha/core/LanguageCode.kt
+++ core/src/main/java/my/noveldokusha/core/LanguageCode.kt
@@ -16,5 +16,11 @@ enum class LanguageCode(
     FRENCH(iso639_1 = "fr", nameResId = R.string.language_french),
     INDONESIAN(iso639_1 = "id", nameResId = R.string.language_indonesian),
     CHINESE(iso639_1 = "zh", nameResId = R.string.language_chinese),
-    RUSSIAN(iso639_1 = "ru", nameResId = R.string.language_russian)
+    RUSSIAN(iso639_1 = "ru", nameResId = R.string.language_russian),
+    VIETNAMESE(iso639_1 = "vi", nameResId = R.string.language_vietnamese),
+    GERMAN(iso639_1 = "de", nameResId = R.string.language_german),
+    TURKISH(iso639_1 = "tr", nameResId = R.string.language_turkish),
+    POLISH(iso639_1 = "pl", nameResId = R.string.language_polish),
+    // JAPANESE(iso639_1 = "ja", nameResId = R.string.language_japanese),
+    // KOREAN(iso639_1 = "ko", nameResId = R.string.language_korean)
 }
