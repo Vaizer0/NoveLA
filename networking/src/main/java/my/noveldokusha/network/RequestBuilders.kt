43bb25dd6bbb6e69060a72fcc6b25325964d5dbf
HnDK0 <212071365+HnDK0@users.noreply.github.com>
2026-02-20 05:09:35 +0300

    add postJson

--- networking/src/main/java/my/noveldokusha/network/RequestBuilders.kt
+++ networking/src/main/java/my/noveldokusha/network/RequestBuilders.kt
@@ -1,20 +1,30 @@
 package my.noveldokusha.network
 
+import com.google.gson.Gson
 import okhttp3.CacheControl
 import okhttp3.FormBody
 import okhttp3.Headers
 import okhttp3.Request
 import okhttp3.RequestBody
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.RequestBody.Companion.toRequestBody
 import java.util.concurrent.TimeUnit
 
 private val DEFAULT_CACHE_CONTROL = CacheControl.Builder().maxAge(10, TimeUnit.MINUTES).build()
 private val DEFAULT_HEADERS = Headers.Builder().build()
 private val DEFAULT_BODY: RequestBody = FormBody.Builder().build()
+private val GSON = Gson()
 
 fun Request.Builder.postPayload(scope: FormBody.Builder.() -> Unit): Request.Builder {
     val builder = FormBody.Builder()
     scope(builder)
     return post(builder.build())
+}
+
+fun Request.Builder.postJson(data: Map<String, Any>): Request.Builder {
+    val json = GSON.toJson(data)
+    val body = json.toRequestBody("application/json; charset=UTF-8".toMediaType())
+    return post(body)
 }
 
 fun getRequest(
