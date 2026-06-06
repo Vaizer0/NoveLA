a89a486d91a13f737a58a1e04dd64185aca2734d
aNaruto <r.angka.kusuma@gmail.com>
2026-06-06 14:23:34 +0800

    fix: Add missing Color import in databaseSearch module

--- features/databaseExplorer/src/main/java/my/noveldoksuha/databaseexplorer/databaseSearch/DatabaseSearchBottomSheet.kt
+++ features/databaseExplorer/src/main/java/my/noveldoksuha/databaseexplorer/databaseSearch/DatabaseSearchBottomSheet.kt
@@ -28,6 +28,7 @@ import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.state.ToggleableState
 import androidx.compose.ui.unit.dp
