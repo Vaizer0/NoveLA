34da19285368515b365c36c812d1e29d1154dc45
HnDK0 <212071365+HnDK0@users.noreply.github.com>
2026-06-17 06:12:55 +0300

    fix: correct app version comparison and update checker URL

--- core/src/main/java/my/noveldokusha/core/domain/AppVersion.kt
+++ core/src/main/java/my/noveldokusha/core/domain/AppVersion.kt
@@ -13,11 +13,13 @@ data class AppVersion(val major: Int, val minor: Int, val fix: Int) : Comparable
     }
 
     override fun compareTo(other: AppVersion): Int {
-        if (major == other.major && minor == other.minor && fix == other.fix) return 0
         if (major > other.major) return 1
+        if (major < other.major) return -1
         if (minor > other.minor) return 1
+        if (minor < other.minor) return -1
         if (fix > other.fix) return 1
-        return -1
+        if (fix < other.fix) return -1
+        return 0
     }
 
     override fun toString() = "v$major.$minor.$fix"
